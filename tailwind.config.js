e3b5beb93a70296d35e7ad11dfba022b726eecaf
tonyandy5630 <tonyandy456@gmail.com>
2024-06-04 13:21:36 +0700

    <package> add axios, tanstack query, yup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,jsx,tsx}",
+    "./src/components/**/*.{html,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
