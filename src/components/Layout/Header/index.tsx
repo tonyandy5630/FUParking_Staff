a143b2814833317dd91199d03d60a8b1da625e1b
tonyandy5630 <tonyandy456@gmail.com>
2024-08-11 17:26:38 +0700

    <mod> follow up

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../../../assets/Bai_Logo.png";
 
 const LOGO_WIDTH = 40;
 
@@ -6,7 +7,7 @@ export default function Header() {
   return (
     <div className='flex items-center justify-start min-w-full p-2 bg-primary-text'>
       <div className='flex items-center justify-between'>
-        <img src='./Bai_Logo.png' width={LOGO_WIDTH} height={LOGO_WIDTH} />
+        <img src={logo} width={LOGO_WIDTH} height={LOGO_WIDTH} />
         <p className='font-bold text-white uppercase text-md'>
           PARKING MANAGEMENT SYSTEM
         </p>
